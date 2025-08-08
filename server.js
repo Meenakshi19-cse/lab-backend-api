54eacceecf972037fba1538982b3d40d371f7ae5
Himanix10 <himanix10@gmail.com>
2025-08-08 15:24:56 +0530

    Tested API endpoints successfully

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://MeenakshiH:Devaki%402018@cluster0.iplarww.mongodb.net/Meenu?retryWrites=true&w=majority&appName=Cluster0',
+ {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log(err));
+
+const itemRoutes = require('./routes/itemRoutes');
+app.use('/api/items', itemRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
